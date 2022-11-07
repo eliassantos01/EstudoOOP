19efc63597037c26f5323c6e87367c5067b74685
eliassantos01 <eliasdossantos2010@gmail.com>
2022-11-07 12:22:39 -0300

    Adicionar arquivos de projeto.

---
+++ OOP/Conta.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP
+{
+    public class Conta
+    {
+        public string Nome { get; set; }
+
+        // Somente a classe pode alterar o atributo Saldo (private)
+        private double Saldo { get; set; }
+        public double Limite { get; private set; }
+        public int Numero { get; set; }
+
+        public void Depositar(double valor)
+        {
+            this.Saldo += valor;
+        }
+
+        public bool Sacar(double valor)
+        {
+            double saldoDisponivel = this.ConsultaSaldoDisponivel();
+            if (valor > saldoDisponivel)
+            {
+                Console.WriteLine("Saldo insuficente");
+                return false;
+            }
+            else
+            {
+                this.Saldo -= valor;
+                return true;
+            }
+
+        }
+
+        public double ConsultaSaldoDisponivel()
+        {
+            return this.Saldo + this.Limite;
+        }
+
+        public void AlterarLimite(double valor)
+        {
+            this.Limite = valor;
+        }
+    }
+}
---
+++ OOP/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace OOP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Aprendendo OOP");
+
+            // Criando uma instância da conta
+            Conta conta = new Conta();
+
+            conta.AlterarLimite(850);
+            conta.Depositar(1000);
+           bool sacar = conta.Sacar(50);
+            if (sacar)
+            {
+                double saldo = conta.ConsultaSaldoDisponivel();
+                Console.WriteLine("Seu saldo total é: R$ {0}", saldo);
+                Console.WriteLine("Seu limite é: R$ {0}", conta.Limite);
+            }
+
+            
+        }
+    }
+}
